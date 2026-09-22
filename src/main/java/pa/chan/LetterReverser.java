4f21c9902935113d65512290f9fc15a1548dfef9
Pa_chan <poghcamb1@gmail.com>
2026-09-23 01:01:27 +0700

    Добавил решение ReverseLetter

--- src/main/java/pa/chan/LetterReverser.java
+++ src/main/java/pa/chan/LetterReverser.java
@@ -1,4 +1,27 @@
 package pa.chan;
 
 public class LetterReverser {
+
+    public String reverse(String text) {
+        char[] chars = text.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {
+                left++;
+            } else if (!Character.isLetter(chars[right])) {
+                right--;
+            } else {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return new String(chars);
+    }
 }
